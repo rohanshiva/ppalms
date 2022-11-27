0d10d14a85b5cd4a84dc51aa82b1a890ce2473d1
Tanush Nadimpalli <Tanushnadimpalli3@gmail.com>
2022-11-27 15:12:14 -0600

    Split generation form tests into individual tests.

--- src/__tests__/GenerationForm.test.tsx
+++ src/__tests__/GenerationForm.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup} from '@testing-library/react';
 import GenerationForm from 'renderer/components/GenerationForm';
 
 import { Router } from 'react-router-dom';
@@ -15,23 +15,30 @@ jest.mock('react-hot-toast', () => ({
   },
 }));
 
-describe('GenerationForm', () => {
-  test('form not being able to submit until all the required inputs have been satisfied', async () => {
-    const props = {
-      location: {
-        state: {
-          codeLines: [],
-          lineTuples: [],
-          editorState: {
-            lineTuples: [],
-            lineTupleStart: null,
-          },
-          editorProps: {
-            code: '',
-          },
-        },
+const props = {
+  location: {
+    state: {
+      codeLines: [],
+      lineTuples: [],
+      editorState: {
+        lineTuples: [],
+        lineTupleStart: null,
       },
-    };
+      editorProps: {
+        code: '',
+      },
+    },
+  },
+};
+
+afterEach(() => {
+  jest.resetAllMocks();
+  cleanup();
+});
+
+describe('GenerationForm', () => {
+
+  const setup = () => {
 
     const history = createMemoryHistory();
 
@@ -40,6 +47,13 @@ describe('GenerationForm', () => {
         <GenerationForm {...props} />
       </Router>
     );
+
+  }
+
+
+  test('form not being able to submit until all the required inputs have been satisfied', async () => {
+
+    setup();
 
     const numberOfProblemsField = screen.getByTestId(
       'problems-field'
@@ -53,6 +67,45 @@ describe('GenerationForm', () => {
     expect(numberOfProblemsField).toBeInvalid();
     expect(problemSetNameField).toBeInvalid();
 
+  });
+
+  test('show error toast when the form is submitted without choosing at least one ppalm problem type.',async () => {
+
+    setup();
+
+    const numberOfProblemsField = screen.getByTestId(
+      'problems-field'
+    ) as HTMLInputElement;
+
+    const problemSetNameField = screen.getByTestId(
+      'problemset-name-field'
+    ) as HTMLInputElement;
+
+    // inflate the number of problems field and problem set name field with valid values
+    await userEvent.type(numberOfProblemsField, '10');
+    await userEvent.type(problemSetNameField, 'Example name');
+
+    const submitBttn = screen.getByTestId('submit-bttn') as HTMLInputElement;
+
+    await userEvent.click(submitBttn);
+
+    // test to see if toast gets triggered when both reorder and mutliple choice checkboxes are not ticked and submit button is clicked
+    expect(toast.error).toBeCalledTimes(1);
+
+  });
+
+  test('Problem set generation should be successful when all the required fields have been filled and the generate button is clicked.',async () => {
+
+    setup();
+
+    const numberOfProblemsField = screen.getByTestId(
+      'problems-field'
+    ) as HTMLInputElement;
+
+    const problemSetNameField = screen.getByTestId(
+      'problemset-name-field'
+    ) as HTMLInputElement;
+
     // inflate the number of problems field and problem set name field with valid values
     await userEvent.type(numberOfProblemsField, '10');
     await userEvent.type(problemSetNameField, 'Example name');
@@ -64,19 +117,15 @@ describe('GenerationForm', () => {
       'multiple-choice-checkbox'
     ) as HTMLInputElement;
 
+    await userEvent.click(multipleChoiceCheckbox);
+    await userEvent.click(reorderCheckbox);
+
     const submitBttn = screen.getByTestId('submit-bttn') as HTMLInputElement;
 
     await userEvent.click(submitBttn);
 
-    // test to see if toast gets triggered when both reorder and mutliple choice checkboxes are not ticked and submit button is clicked
-    expect(toast.error).toBeCalledTimes(1);
-
-    await userEvent.click(multipleChoiceCheckbox);
-    await userEvent.click(reorderCheckbox);
-
-    await userEvent.click(submitBttn);
-
-    // test to see that toast was not called again when at least checkbox was clicked
-    expect(toast.error).toBeCalledTimes(1);
+    // test to see that error toast was not called when at all the required fields where filled.
+    expect(toast.error).toBeCalledTimes(0);
+    
   });
 });
