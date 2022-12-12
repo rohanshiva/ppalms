0471435615765c445012066b41b1ef3d14a5d32d
Ramko9999 <ramapitchala@gmail.com>
2022-12-12 16:52:38 -0600

    [Task 2] add unit test to ensure 'blank fills' are in correct order

--- src/__tests__/api/ProblemSetGenerator.test.ts
+++ src/__tests__/api/ProblemSetGenerator.test.ts
@@ -6,7 +6,7 @@ const mockLineTuples = [{ start: 0, end: 2 }];
 const mockProblemTypes = [ProblemType.REORDER];
 
 describe('ProblemSetGenerator', () => {
-  test('successfully generate a problem set', () => {
+  test('successfully generate a problem set with a single type of problems', () => {
     const mockMaxNumberOfProblems = 2;
     const mockProblemSetName = 'mock_problem_set';
     let problemTypesConfig: any = {};
@@ -23,6 +23,32 @@ describe('ProblemSetGenerator', () => {
     expect(problemSet.name).toEqual(mockProblemSetName);
     expect(problemSet.problems.length).toEqual(mockMaxNumberOfProblems);
     expect(problemSet.problemTypes).toEqual(mockProblemTypes);
+  });
+
+  test('successfully generate a problem set with multiple types of problems', () => {
+    let problemTypesConfig: any = {};
+    problemTypesConfig[ProblemType.REORDER] = 2;
+    problemTypesConfig[ProblemType.FILL_IN_THE_BLANK] = 2;
+
+    const problemSet = ProblemSetGenerator.generate(
+      mockProblemTypes,
+      mockCode,
+      mockLineTuples,
+      problemTypesConfig,
+      'problem_set'
+    ) as ProblemSet;
+
+    expect(problemSet.name).toEqual('problem_set');
+    expect(problemSet.problems.length).toEqual(4);
+    expect(
+      problemSet.problems.filter(
+        (prob) => prob.type === ProblemType.FILL_IN_THE_BLANK
+      ).length
+    ).toBe(2);
+    expect(
+      problemSet.problems.filter((prob) => prob.type === ProblemType.REORDER)
+        .length
+    ).toBe(2);
   });
 
   test.each([
